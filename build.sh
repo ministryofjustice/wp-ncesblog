6984919c902f686f57a040eec1a7fac810fd21d1
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-03-09 16:27:35 +0000

    Upgrade WordPress and configure composer auth.

--- build.sh
+++ build.sh
@@ -9,5 +9,20 @@
 # Clean up the working directory (useful when building from local dev files)
 git clean -xdf
 
+# Add composer auth file
+if [ ! -z $COMPOSER_USER ] && [ ! -z $COMPOSER_PASS ]
+then
+	cat <<- EOF >> auth.json
+		{
+			"http-basic": {
+				"composer.wp.dsd.io": {
+					"username": "$COMPOSER_USER",
+					"password": "$COMPOSER_PASS"
+				}
+			}
+		}
+	EOF
+fi
+
 # Install PHP dependencies (WordPress, plugins, etc.)
 composer install
